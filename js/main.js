17558b3451b6ef02fa1a4c08deb4434a2b33caeb
Sebastien Andary <sebastien.andary@gmail.com>
2016-09-13 21:14:11 +0200

    navigation arrow now go to top of the page when last section is selected

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@ $(document).ready(function () {
     $("header").load("/partials/header.html", function () {
         var $navList = $("#navigation > ul");
         var $sections = $("section");
-
+        var $navArrow = $('.nav-arrow');
+        
         if ($sections.length > 1) {
             $sections.each(function (i, elem) {
                 var $anchor;
@@ -22,14 +23,47 @@ $(document).ready(function () {
                 $navList.append(listItem);
             });
             
+            // selected nav item is first section on page load
             $navList.find("li").first().find("span").addClass("current-section");
         }
-        
-        var afterScrollHandler = function (index, sections) {
-            $navList.find("span").removeClass("current-section");
-            $navList.find("li[data-section-name='" + sections[index].data("section-name") + "'] span").addClass("current-section");
+
+        var setDefaultNavArrowClickHandler = function () {
+            var link = $navArrow.find('a');
+            link.unbind('click');
+            link.on('click', function () {
+                console.log("goto next");
+                $.scrollify.next();
+            });
         };
         
+        var setLastSectionNavArrowClickHandler = function () {
+            var link = $navArrow.find('a');
+            link.unbind('click');
+            link.on('click', function () {
+                console.log("goto first");
+                $.scrollify.move(0);
+            });
+        };
+        
+        
+        var afterScrollHandler = function (index, sections) {
+            
+            // set selected nav item class
+            $navList.find("span").removeClass("current-section");
+            $navList.find("li[data-section-name='" + sections[index].data("section-name") + "'] span").addClass("current-section");
+            
+            // nav arrow scrolls to top section when current section is the last one
+            if (index === sections.length - 1) {
+                $navArrow.find('i').addClass("last-section");
+                setLastSectionNavArrowClickHandler();
+            } else {
+                $navArrow.find('i').removeClass("last-section");    
+                setDefaultNavArrowClickHandler();
+            }
+        };
+        
+        setDefaultNavArrowClickHandler();
+
         $.scrollify({
             section : "section",
             sectionName : "section-name",
