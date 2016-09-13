3516a715bd528c3d4879768d19ff1d64cb1b5a55
TristanLG <tristan@naturalpad.fr>
2016-09-13 17:28:27 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,44 @@
+"use strict";
+
+$(document).ready(function(){
+    $("header").load("/partials/header.html", function(){
+        var $navList = $("#navigation > ul");
+        var $sections = $("section");
+        
+        if($sections.length > 1){
+            $sections.each(function(i, elem){
+                var $anchor;
+                var $elem = $(elem);
+                
+                $elem.data("section-name", $elem.attr("title"));
+                var listItem = $("<li class='nav-item'>")
+                .html($anchor = $("<a href='javascript:void(0);'>")
+                      .html($("<span>")
+                            .html($elem.attr("title"))));
+                $anchor.on("click", function(){
+                    $.scrollify.move("#" + $elem.data("section-name"))
+                });
+                $navList.append(listItem);
+            });     
+        }
+        
+        var currentSection = 0;
+        
+        $.scrollify({
+            section : "section",
+            sectionName : "section-name",
+            interstitialSection : "",
+            easing: "easeOutExpo",
+            scrollSpeed: 1100,
+            offset : 0,
+            scrollbars: true,
+            standardScrollElements: "",
+            setHeights: true,
+            overflowScroll: true,
+            before:function() {},
+            after:function() {},
+            afterResize:function() {},
+            afterRender:function() {}
+        });    
+    });
+});
