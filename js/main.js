e43f769b4d11ed790adb7d20a8d17da481bfab7b
Sebastien Andary <sebastien.andary@gmail.com>
2016-09-13 20:44:20 +0200

    adds css class to selected section navigation item

--- js/main.js
+++ js/main.js
@@ -1,26 +1,35 @@
 "use strict";
 
-$(document).ready(function(){
-    $("header").load("/partials/header.html", function(){
+$(document).ready(function () {
+    $("header").load("/partials/header.html", function () {
         var $navList = $("#navigation > ul");
         var $sections = $("section");
-        
-        if($sections.length > 1){
-            $sections.each(function(i, elem){
+
+        if ($sections.length > 1) {
+            $sections.each(function (i, elem) {
                 var $anchor;
                 var $elem = $(elem);
+                var sectionName = $elem.data("section-name");
                 
                 var listItem = $("<li class='nav-item'>")
+                .attr("data-section-name", sectionName)                            
                 .html($anchor = $("<a href='javascript:void(0);'>")
                       .html($("<span>")
                             .html($elem.attr("title"))));
-                $anchor.on("click", function(){
-                    $.scrollify.move("#" + $elem.data("section-name"))
+                $anchor.on("click", function () {
+                    $.scrollify.move("#" + sectionName)
                 });
                 $navList.append(listItem);
-            });     
+            });
+            
+            $navList.find("li").first().find("span").addClass("current-section");
         }
-
+        
+        var afterScrollHandler = function (index, sections) {
+            $navList.find("span").removeClass("current-section");
+            $navList.find("li[data-section-name='" + sections[index].data("section-name") + "'] span").addClass("current-section");
+        };
+        
         $.scrollify({
             section : "section",
             sectionName : "section-name",
@@ -32,10 +41,10 @@ $(document).ready(function(){
             standardScrollElements: "",
             setHeights: true,
             overflowScroll: true,
-            before:function() {},
-            after:function() {},
-            afterResize:function() {},
-            afterRender:function() {}
+            before: function () {},
+            after: afterScrollHandler,
+            afterResize: function () {},
+            afterRender: function () {}
         });
     });
 });
